7058c7cded45e23c8108539ff11e03578ca6713b
RMDbytes <siddyf5@gmail.com>
2026-09-21 13:51:30 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Lets the app be installed and opens the last page if the connection drops.
-const CACHE = 'football-shell-v7';
+const CACHE = 'football-shell-v8';
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(['/'])).then(() => self.skipWaiting()));
 });
